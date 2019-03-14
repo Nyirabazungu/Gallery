b1b11b061d2ef39a5fb280a21fb4b1093ec5f0cd
Nyirabazungu <vnyanki6@gmail.com>
2019-03-14 18:42:39 +0200

    add class

--- images/views.py
+++ images/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 def welcome(request):
     return render(request, 'welcome.html')
 
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_categories = Category.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-images/search.html',{"message":message,"categories": searched_categories})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-images/search.html',{"message":message})
+
+
+def category(request,category_id):
+    try:
+        category = Category.objects.get(id = category_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-news/category.html", {"category":category})       
