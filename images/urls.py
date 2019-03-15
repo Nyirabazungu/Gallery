bb2e34c4aaf4369c1129087d4702a0603986f5ab
Nyirabazungu <vnyanki6@gmail.com>
2019-03-15 10:08:58 +0200

    add search

--- images/urls.py
+++ images/urls.py
@@ -3,11 +3,14 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns=[
+urlpatterns=[  
     url('^$',views.welcome,name = 'welcome'),
+    url(r'^images/(\d+)',views.images,name ='images'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^category/(\d+)',views.category,name ='category')
+
    ]
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)  
+
+
